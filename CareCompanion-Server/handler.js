759a99508af95c928d99ea539efbf6f761a04e66
Adrian Ziegler <adrianziegler@web.de>
2017-06-18 00:16:00 +0200

    added dynamo db and post req wip

--- CareCompanion-Server/handler.js
+++ CareCompanion-Server/handler.js
@@ -1,7 +1,13 @@
 'use strict';
 
+console.log('Loading functions');
+
+const doc = require('dynamodb-doc');
+
+const dynamo = new doc.DynamoDB();
+
 module.exports.functionName = (event, context, callback) => {
-	console.log(event);
+  console.log('Received event:', JSON.stringify(event, null, 2));
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -13,4 +19,17 @@ module.exports.functionName = (event, context, callback) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.importData = (event, context, callback) => {
+  console.log('Received event:', JSON.stringify(event, null, 2))
+  //dynamo.putItem(JSON.parse(event.body), done);
+  const response = {
+    statusCode: 200, 
+     body: JSON.stringify({
+           message: 'Go Serverless v1.0! Your function executed successfully!'
+      }),
+  };
+  
+  callback(null, response);
 };
