029034edfba69dc78840b8f862a6d52f42ea9958
Marcel Engelmann <mail@marcelengelmann.de>
2017-06-17 23:30:01 +0200

    App update

--- CareCompanion-App/src/pages/add-patient/add-patient.ts
+++ CareCompanion-App/src/pages/add-patient/add-patient.ts
@@ -1,12 +1,8 @@
-import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, Input} from '@angular/core';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
 
-/**
- * Generated class for the AddPatientPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-add-patient',
@@ -14,11 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddPatientPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  myInputName: any;
+  myInputStreet: any;
+  myInputZip: any;
+  myInputCity: any;
+  myInputCountry: any;
+  myInputDiseases: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: Storage
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddPatientPage');
   }
+
+  send = () => {
+    this.storage.set('paitent', {
+      "name" : this.myInputName,
+      "street" : this.myInputStreet,
+      "zip" : this.myInputZip,
+      "city" : this.myInputCity,
+      "country" : this.myInputCountry,
+      "diseases" : this.myInputDiseases
+    });
+    console.log('Form was submitted');
+    console.log(this.myInputName);
+  }
+
+
 
 }
--- CareCompanion-App/src/pages/home/home.ts
+++ CareCompanion-App/src/pages/home/home.ts
@@ -11,22 +11,39 @@ import {Observable} from 'rxjs/Rx';
 })
 export class HomePage {
 
+  searchActive:boolean;
+  paitentData:any;
+
   constructor(
     public navCtrl: NavController,
     private http: Http,
     public storage: Storage
   ) {
 
-
-    Observable.interval(1000).subscribe(x => {
+    Observable.interval(5000).subscribe(x => {
       this.getPaitenInfo();
+    });
+
+    this.searchActive = false;
+
+    storage.get('paitent').then((val) => {
+      console.log(val);
+      this.paitentData = val;
+      console.log(this.paitentData.name);
     });
   }
 
   getPaitenInfo = () => {
-
     console.log('Send request');
-
+    // https://hpk5wsr3md.execute-api.us-east-1.amazonaws.com/dev/getdata
+    this.http.get("https://hpk5wsr3md.execute-api.us-east-1.amazonaws.com/dev/getdata")
+      .subscribe(data => {
+        console.log('Response');
+        console.log(data);
+      }, error => {
+        console.log(JSON.stringify(error.json()));
+      }
+    );
   }
 
   addPatient = () => {
@@ -36,6 +53,22 @@ export class HomePage {
 
   search = () => {
     console.log('click');
+    if(this.searchActive == false){
+      this.searchActive = true;
+      console.log(this.searchActive);
+    }
+    else{
+      this.searchActive = false;
+      console.log(this.searchActive);
+    }
+  }
+
+  onInput = () => {
+    console.log('search');
+  }
+
+  ionCancel = () => {
+    console.log('search');
   }
 
 }
