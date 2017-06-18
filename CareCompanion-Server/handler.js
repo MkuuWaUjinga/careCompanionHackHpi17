de1789bd13b8452a7fc8861f4a36e5e907056810
Adrian Ziegler <adrianziegler@web.de>
2017-06-18 09:42:07 +0200

    dynamo synch works, post and get requests finished

--- CareCompanion-Server/handler.js
+++ CareCompanion-Server/handler.js
@@ -7,37 +7,46 @@ const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
 
 module.exports.functionName = (event, context, callback) => {
-  console.log('Received event:', JSON.stringify(event, null, 2));
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
+  dynamo.getItem({
+    TableName: "CareCompanion",
+    Key: {
+      patientId: "1"
+    }}
+    ).promise()
+  .then(item => {
+    console.log(item.Item);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
       patientId: 1,
-      inFlat: 0, 
-      heartRate: 100,
-      random: 55
-    }),
-  };
-
+      inFlat: item.Item.data.beaconconnected, 
+      heartRate: item.Item.data.pulse,
+      latitude: item.Item.data.latitude,
+      longitude: item.Item.data.latitude
+      }),
+    };
   callback(null, response);
+  });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
 module.exports.importData = (event, context, callback) => {
   console.log('Received event:', event);
-  var dictionary = {};
-  dictionary["Item"] = event.queryStringParameters;
-  dictionary["TableName"] = "CareCompanion";
-  dynamo.putItem(dictionary);
-  console.log(dynamo.getItem({"Key":{"patientId" : {
-  "beac":N}}, "TableName" : "CareCompanion"}))
-  const response = {
-    statusCode: 200,  
-     body: JSON.stringify({
+  dynamo.putItem({
+        TableName: "CareCompanion",
+        Item: {
+          patientId: "1",
+          data: event.queryStringParameters
+        }
+    }).promise()
+    .then(item => {
+      console.log(item);
+      const response = {
+        statusCode: 200,  
+        body: JSON.stringify({
            message: 'Go Serverless v1.0! Your function executed successfully!'
-      }),
-  };
-  
-  callback(null, response);
+        }),
+      };
+      callback(null, response);
+    });
 };
