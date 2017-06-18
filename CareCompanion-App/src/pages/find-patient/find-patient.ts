f062c792af1b72369de73b88bd2cd0c6264faa71
Marcel Engelmann <mail@marcelengelmann.de>
2017-06-18 11:03:37 +0200

    Update app

--- CareCompanion-App/src/pages/find-patient/find-patient.ts
+++ CareCompanion-App/src/pages/find-patient/find-patient.ts
@@ -19,62 +19,7 @@ export class FindPatientPage {
     public navParams: NavParams,
     public geolocation: Geolocation
   ) {
-    this.loadMap();
-
-    setInterval(() => {
-      this.loadMap();
-    }, 2000);
 
   }
-
-  loadMap(){
-
-      let latLng = new google.maps.LatLng(52.3935678,13.1317739);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true,
-        zoomControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        rotateControl: false,
-        fullscreenControl: false,
-        timeout: 10000,
-        enableHighAccuracy: true
-      }
-
-      this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-      this.addMarker();
-  }
-
-  addMarker(){
-
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
-    });
-
-    let content = "<h4>Information!</h4>";
-
-    this.addInfoWindow(marker, content);
-
-  }
-
-  addInfoWindow(marker, content){
-
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
-
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
-
-}
 
 }
